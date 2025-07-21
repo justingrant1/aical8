c9285842a19ec5009f8de4d63d593e1f73d0875a
justingrant1 <jgrant@trestacapital.com>
2025-07-20 22:48:08 -0400

    Add diagnostic message to config/boot.rb to debug Logger issue

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,6 @@
 require 'logger'
+puts "Logger defined in boot.rb? #{defined?(Logger)}"
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-
-
